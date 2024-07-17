7a39e89064a71dcfcaab36e55abd26893462138e
lalit-shriffle <lalitp@shriffle.com>
2024-07-17 18:57:52 +0530

    added date and math files  methodsarithmetic and time operation

---
+++ 10_date.js
@@ -0,0 +1,7 @@
+const date = new Date();
+console.log(date);
+
+const date1 = new Date("2002-02-22");
+console.log(date1);
+
+console.log(new Date("year","month"));
---
+++ 11_math.js
@@ -0,0 +1,29 @@
+console.log(typeof Math); // object
+
+console.log(Math.PI);
+
+// round a float number
+console.log(Math.round(2.33));
+
+// sing of number
+console.log(Math.sign(4));
+console.log(Math.sign(-4));
+console.log(Math.sign(0));
+
+
+// power of number
+console.log(Math.pow(2,3));
+
+// square root 
+console.log(Math.sqrt(9));
+
+
+// minimum value in list
+console.log(Math.min(11,23,4,55));
+
+// maximum value in list
+console.log(Math.max(22,3,4,4));
+
+// generate no. bw 0 to 1
+console.log(Math.random());
+
